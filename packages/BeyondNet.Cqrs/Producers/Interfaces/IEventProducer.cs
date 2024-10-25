b6d70b54d652d08467e008e7529e38eb2e82a75c
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2024-10-25 10:30:20 -0500

    Refactoring

---
+++ packages/BeyondNet.Cqrs/Producers/Interfaces/IEventProducer.cs
@@ -0,0 +1,9 @@
+﻿using BeyondNet.Ddd.Interfaces;
+
+namespace BeyondNet.Cqrs.Producers.Interfaces
+{
+    public interface IEventProducer
+    {
+        Task ProduceAsync<T>(string topic, T @event) where T : IDomainEvent;
+    }
+}
