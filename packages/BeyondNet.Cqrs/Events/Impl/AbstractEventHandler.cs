c29429ccfa050313160a32255119c54155a963db
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2024-10-26 00:19:54 -0500

    Refactoring

---
+++ packages/BeyondNet.Cqrs/Events/Impl/AbstractEventHandler.cs
@@ -0,0 +1,43 @@
+﻿using BeyondNet.Cqrs.Events.Interfaces;
+using BeyondNet.Ddd.Interfaces;
+
+namespace BeyondNet.Cqrs.Events.Impl
+{
+    public abstract class AbstractEventHandler<TEvent> : IEventHandler<TEvent> where TEvent : IDomainEvent
+    {
+        public readonly ILogger<AbstractEventHandler<TEvent>> logger;
+
+        protected abstract Task Handle(TEvent @event);
+        
+        protected AbstractEventHandler(ILogger<AbstractEventHandler<TEvent>> logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public Task On(TEvent @event)
+        {
+            logger.LogInformation($"Handling event {@event.GetType().Name}");
+
+            try
+            {
+                logger.LogInformation($"Handling event {@event.GetType().Name}");
+
+                Handle(@event);
+
+                logger.LogInformation($"Event handled {@event.GetType().Name}");
+
+                return Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Error handling event {@event.GetType().Name}");
+                throw;
+            }
+            finally
+            {
+                logger.LogInformation($"Event handled {@event.GetType().Name}");
+
+            }
+        }
+    }
+}
---
+++ packages/BeyondNet.Cqrs/Events/Interfaces/IEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using BeyondNet.Ddd.Interfaces;
+
+namespace BeyondNet.Cqrs.Events.Interfaces
+{
+    public interface IEventHandler<TEvent> where TEvent : IDomainEvent
+    {
+        Task On(TEvent @event);
+    }
+}
